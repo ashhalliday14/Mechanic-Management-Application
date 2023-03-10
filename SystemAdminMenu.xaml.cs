baf653b859049a23fd04dc9888b03d70cc5d26f6
ashhalliday14 <87982373+ashhalliday14@users.noreply.github.com>
2023-03-10 14:01:07 +0000

    jobs and tasks on admin dashboard

--- SystemAdminMenu.xaml.cs
+++ SystemAdminMenu.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -15,6 +14,7 @@ using AdvancedProgramming.Contracts;
 using AdvancedProgramming.Models;
 using AdvancedProgramming.Data;
 using Unity;
+using DatabaseExample.Models;
 
 namespace AdvancedProgramming
 {
@@ -25,9 +25,61 @@ namespace AdvancedProgramming
     {
         User loggedInUser;
         List<string> messages;
+
+        //IRepositories for all elements of the job
+        IRepository<Customer> customerContext;
+        IRepository<Job> jobContext;
+        IRepository<User> userContext;
+        IRepository<AssignedTo> assignedToContext;
+        IRepository<Completed> completedContext;
+        IRepository<Task> taskContext;
+
+        //create a list for customers
+        List<Customer> customersList;
+        Customer selectedCustomer;
+        int customerListsSize = 0;
+        int customerPosition = 0;
+
+        //create a list for jobs
+        List<Job> jobsList;
+        Job selectedJob;
+        int jobListSize = 0;
+        int jobPosition = 0;
+
+        //create a list for users
+        List<User> usersList;
+        User selectedUser;
+        int userListSize = 0;
+        int userPosition = 0;
+
+        //create a list for assigned to status
+        List<AssignedTo> assignedTosList;
+        AssignedTo selectedAssignedTo;
+        int assignedToListSize = 0;
+        int assignedToPosition = 0;
+
+        //create a list for completed statuss
+        List<Completed> completedsList;
+        Completed seletedCompleted;
+        int completedListSize = 0;
+        int completedPosition = 0;
+
+        //create a list for tasks
+        List<Task> tasksList;
+        Task selectedTask;
+        int taskListSize = 0;
+        int taskPosition = 0;
         public SystemAdminMenu(User u)
         {
             loggedInUser = u;
+
+            this.userContext = ContainerHelper.Container.Resolve<IRepository<User>>();
+            this.customerContext = ContainerHelper.Container.Resolve<IRepository<Customer>>();
+            this.jobContext = ContainerHelper.Container.Resolve<IRepository<Job>>();
+            this.assignedToContext = ContainerHelper.Container.Resolve<IRepository<AssignedTo>>();
+            this.completedContext = ContainerHelper.Container.Resolve<IRepository<Completed>>();
+            this.taskContext = ContainerHelper.Container.Resolve<IRepository<Task>>();
+
             InitializeComponent();
             LoadMessages();
         }
@@ -83,6 +135,152 @@ namespace AdvancedProgramming
             var random = new Random();
             int index = random.Next(messages.Count);
             lblMessage.Text = messages[index];
+        }
+
+        private void RefreshJobs(object sender, RoutedEventArgs e)
+        {
+            List<Customer> customerList = customerContext.Collection().ToList();
+            cmbCustomer.ItemsSource = customerList;
+            cmbCustomer.DisplayMemberPath = "Name";
+            cmbCustomer.SelectedValuePath = "Id";
+
+            customersList = customerContext.Collection().ToList();
+            customerListsSize = customersList.Count();
+
+            selectedCustomer = customersList.FirstOrDefault();
+            customerPosition = customersList.IndexOf(selectedCustomer);
+
+
+            List<Job> jobList = jobContext.Collection().ToList();
+            jobsList = jobContext.Collection().ToList();
+            jobListSize = jobsList.Count();
+
+            selectedJob = jobsList.FirstOrDefault();
+            jobPosition = jobsList.IndexOf(selectedJob);
+
+            List<AssignedTo> assignedToList = assignedToContext.Collection().ToList();
+            cmbAssignedTo.ItemsSource = assignedToList;
+            cmbAssignedTo.DisplayMemberPath = "Name";
+            cmbAssignedTo.SelectedValuePath = "Id";
+
+            assignedTosList = assignedToContext.Collection().ToList();
+            assignedToListSize = assignedTosList.Count();
+
+            selectedAssignedTo = assignedTosList.FirstOrDefault();
+            assignedToPosition = assignedTosList.IndexOf(selectedAssignedTo);
+
+            List<Completed> completedList = completedContext.Collection().ToList();
+            cmbCompleted.ItemsSource = completedList;
+            cmbCompleted.DisplayMemberPath = "Name";
+            cmbCompleted.SelectedValuePath = "Id";
+
+            completedsList = completedContext.Collection().ToList();
+            completedListSize = completedsList.Count();
+
+            seletedCompleted = completedsList.FirstOrDefault();
+            completedPosition = completedsList.IndexOf(seletedCompleted);
+
+            //set values of fields 
+            cmbCustomer.SelectedValue = selectedJob.CustomerName;
+            txtJobDescription.Text = selectedJob.Description;
+            cmbAssignedTo.SelectedValue = selectedAssignedTo.Id;
+            cmbCompleted.SelectedValue = seletedCompleted.Id;
+        }
+
+        private void PreviousRecord(object sender, RoutedEventArgs e)
+        {
+            if (jobPosition != 0)
+            {
+                selectedJob = jobsList[jobPosition - 1];
+                jobPosition = jobsList.IndexOf(selectedJob);
+
+                cmbCustomer.SelectedValue = selectedJob.CustomerName;
+                txtJobDescription.Text = selectedJob.Description;
+                cmbAssignedTo.SelectedValue = selectedAssignedTo.Id;
+                cmbCompleted.SelectedValue = seletedCompleted.Id;
+            }
+        }
+
+        private void NextRecord(object sender, RoutedEventArgs e)
+        {
+            if (jobPosition != jobListSize - 1)
+            {
+                jobPosition = jobListSize - 1;
+                selectedJob = jobsList[jobPosition];
+
+                cmbCustomer.SelectedValue = selectedJob.CustomerName;
+                txtJobDescription.Text = selectedJob.Description;
+                cmbAssignedTo.SelectedValue = selectedAssignedTo.Id;
+                cmbCompleted.SelectedValue = seletedCompleted.Id;
+            }
+        }
+
+        private void RefreshTasks(object sender, RoutedEventArgs e)
+        {
+            List<AssignedTo> assignedToList = assignedToContext.Collection().ToList();
+            cmbAssignedTo2.ItemsSource = assignedToList;
+            cmbAssignedTo2.DisplayMemberPath = "Name";
+            cmbAssignedTo2.SelectedValuePath = "Id";
+
+            assignedTosList = assignedToContext.Collection().ToList();
+            assignedToListSize = assignedTosList.Count();
+
+            selectedAssignedTo = assignedTosList.FirstOrDefault();
+            assignedToPosition = assignedTosList.IndexOf(selectedAssignedTo);
+
+            List<Completed> completedList = completedContext.Collection().ToList();
+            cmbCompleted2.ItemsSource = completedList;
+            cmbCompleted2.DisplayMemberPath = "Name";
+            cmbCompleted2.SelectedValuePath = "Id";
+
+            completedsList = completedContext.Collection().ToList();
+            completedListSize = completedsList.Count();
+
+            seletedCompleted = completedsList.FirstOrDefault();
+            completedPosition = completedsList.IndexOf(seletedCompleted);
+
+
+            List<Task> taskList = taskContext.Collection().ToList();
+
+            tasksList = taskContext.Collection().ToList();
+            taskListSize = tasksList.Count();
+
+            selectedTask = tasksList.FirstOrDefault();
+            taskPosition = tasksList.IndexOf(selectedTask);
+
+            //set values of fields 
+            txtTaskName.Text = selectedTask.TaskName;
+            txtDescription.Text = selectedTask.Description;
+            cmbAssignedTo2.SelectedValue = selectedAssignedTo.Id;
+            cmbCompleted2.SelectedValue = seletedCompleted.Id;
+        }
+
+        private void PreviousRecord2(object sender, RoutedEventArgs e)
+        {
+            if (taskPosition != 0)
+            {
+                selectedTask = tasksList[taskPosition - 1];
+                taskPosition = tasksList.IndexOf(selectedTask);
+
+                txtTaskName.Text = selectedTask.TaskName;
+                txtDescription.Text = selectedTask.Description;
+                cmbAssignedTo2.SelectedValue = selectedAssignedTo.Id;
+                cmbCompleted2.SelectedValue = seletedCompleted.Id;
+            }
+        }
+
+        private void NextRecord2(object sender, RoutedEventArgs e)
+        {
+            if (taskPosition != taskListSize - 1)
+            {
+                taskPosition = taskListSize - 1;
+                selectedTask = tasksList[taskPosition];
+
+                txtTaskName.Text = selectedTask.TaskName;
+                txtDescription.Text = selectedTask.Description;
+                cmbAssignedTo2.SelectedValue = selectedAssignedTo.Id;
+                cmbCompleted2.SelectedValue = seletedCompleted.Id;
+            }
         }
 
         private void Exit(object sender, RoutedEventArgs e)
